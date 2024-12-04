106ba9b3d3ce74e30a0cf051ddb37d5391fb33be
amitbazarh12 <123497491+amitbazarh12@users.noreply.github.com>
2024-12-04 19:40:53 +0000

    section 2 completed up until feature creation

--- sections1and2.R
+++ sections1and2.R
@@ -1,47 +1,81 @@
-"""
+#-------------------------------------------------------------------------------
 
-Data Preprocessing
-
-Sections 1 and Sections 2
-"""
+#SECTION 1 - DATA INSPECTION
 
 #Loading the entire dataset
-completeLoanData <- read.csv('completeLoanData.csv', na.strings = c("NA", ""))
+completeLoanData <- read.csv('completeLoanData.csv')
 
 #Checking the data type of every feature 
-#str(completeLoanData)
+str(completeLoanData)
 
-# handling missing values
-#summary(completeLoanData)
+#-------------------------------------------------------------------------------
 
-
-LoanData_NA_count <- apply(is.na(completeLoanData), 2, sum)
-LonData_NA_perc <- LoanData_NA_count / dim(completeLoanData)[1] * 100
-LonData_NA_perc
+#SECTION 2 - DATA UNDERSTANDING, PRE-PROCESSING AND EXPLORATION 
 
 
+#FEATURE DELETION
 
-#preparing training/test set
-n_rows <- nrow(completeLoanData)
-training_idx <- sample(n_rows, n_rows * 0.7)
-training_data <- completeLoanData[training_idx,]
-test_data <- completeLoanData[-training_idx,]
+"
+- dropping 'Id' feature due to its irrelevance to our problem statement
+- dropping 'CITY' feature to reduce noise and simplify data
+"
+completeLoanData <- completeLoanData[, !names(completeLoanData) %in% c("Id", "CITY")]
+
+#EXPLORATORY DATA ANALYSIS AND DETECTED ERRORS 
+
+"
+- checking the dataset for missing field values 
+- checking the categorical features with string data types for possible duplicate values and correcting them accordingly. 
+- checking the 'Income' feature for outliers 
+"
+loan_data_empty_count <- apply(completeLoanData == "" | is.na(completeLoanData),2,sum)
+loan_data_empty_count
 
 
-str(training_data)
-str(test_data)
+unique_marriage <- unique(completeLoanData$Married.Single)
+unique_marriage
 
-#data for training data
-write.csv(training_data,
-        file = "training_data.csv",
-        row.names = TRUE,
-        na = "",
-        quote = TRUE)
+unique_house_ownership <- unique(completeLoanData$House_Ownership)
+unique_house_ownership
 
+unique_car_ownership <- unique(completeLoanData$Car_Ownership)
+unique_car_ownership
 
-#data for test data
-write.csv(test_data,
-        file = "test_data.csv",
-        row.names = TRUE,
-        na = "",
-        quote = TRUE)
+unique_profession <- unique(completeLoanData$Profession)
+unique_profession
+
+unique_states <- unique(completeLoanData$STATE)
+unique_states
+library(stringr)
+completeLoanData$STATE <- trimws(completeLoanData$STATE)
+completeLoanData$STATE <- str_replace_all(completeLoanData$STATE, "Uttar_Pradesh\\[5\\]", "Uttar_Pradesh")
+unique_states
+
+summary(completeLoanData$Income)
+
+hist(completeLoanData$Income, 
+     main = "Income Distribution", 
+     xlab = "Income", 
+     ylab = "Frequency",
+     col="darkgreen",
+     )
+
+dev.off()
+
+boxplot(completeLoanData$Income, 
+        main = "Income Distribution", 
+        xlab = "Income", 
+        ylab = "Values", 
+        col = "darkorange", 
+        horizontal = TRUE)
+
+dev.off()
+
+plot(density(completeLoanData$Income), 
+     main = "Income Distribution", 
+     xlab = "Income", 
+     ylab = "Density", 
+     col = "blue", 
+     lwd = 1.5) 
+
+dev.off()
